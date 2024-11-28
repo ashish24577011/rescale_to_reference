29e6de19713f4364b3d83a7eb2593ab494e1e15e
ashish24577011 <ashish_g@bt.iitr.ac.in>
2024-11-28 11:05:11 +0530

    python_code.py

--- scripts/python_code.py
+++ scripts/python_code.py
@@ -1,28 +1,28 @@
 import sys
 import pandas as pd
 
-# Command-line arguments
+
 freq_file = sys.argv[1]
 merged_file = sys.argv[2]
 output_file = sys.argv[3]
 
-# Load frequency and reference data
+
 freq_data = pd.read_csv(freq_file, sep='\t', header=None, names=['length', 'frequency'])
 ref_data = pd.read_csv('data/reference.hist', sep='\t', header=None, names=['length', 'norm_freq'])
 
-# Find the reference length with highest normalized frequency
+
 max_norm_freq = ref_data['norm_freq'].max()
 target_length = ref_data.loc[ref_data['norm_freq'] == max_norm_freq, 'length'].values[0]
 target_frequency = freq_data.loc[freq_data['length'] == target_length, 'frequency'].values[0]
 
-# Calculate required line counts for each length
+
 required_lines = {
     length: round((norm_freq / max_norm_freq) * target_frequency)
     for length, norm_freq in zip(ref_data['length'], ref_data['norm_freq'])
     if length in freq_data['length'].values
 }
 
-# Extract lines from merged file
+
 extracted_lines = []
 with open(merged_file, 'r') as infile:
     for line in infile:
@@ -32,7 +32,7 @@ with open(merged_file, 'r') as infile:
             extracted_lines.append(line)
             required_lines[length] -= 1
 
-# Write output
+
 with open(output_file, 'w') as outfile:
     outfile.writelines(extracted_lines)
 
